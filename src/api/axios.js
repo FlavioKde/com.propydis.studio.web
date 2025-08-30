285e27e17f6c4319acfa48f9c9212a89f0f8d893
FlavioKde <flaviodavirro@gmail.com>
2025-08-30 02:08:19 +0200

    Config tailwind, with problems. Loginjsx, postcss.config.jsx, tailwind.config.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -6,4 +6,13 @@ const api = axios.create({
 
 });
 
+api.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token && config.url !== '/auth/login' && config.url !== '/auth/register') {
+    config.headers.Authorization = `Bearer ${token}`;
+
+  }
+  return config;
+});
+
 export default api;   
